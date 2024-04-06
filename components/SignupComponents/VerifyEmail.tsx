ae9423f14c1dd59e4089f01c615d7dde08066ebe
Dave Gentilli <dgentilli@gmail.com>
2024-04-06 13:35:37 +0300

    Implement UI elements for the VerifyEmail Component

--- components/SignupComponents/VerifyEmail.tsx
+++ components/SignupComponents/VerifyEmail.tsx
@@ -1,6 +1,8 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { SignupStep } from '../../constants/signup';
 import ScreenWrapper from '../../baseComponents/ScreenWrapper';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Spacer from '../../baseComponents/Spacer';
 
 interface VerifyEmailScreenProps {
   setStep: (step: SignupStep) => void;
@@ -8,18 +10,61 @@ interface VerifyEmailScreenProps {
 
 const VerifyEmail = (props: VerifyEmailScreenProps) => {
   const { setStep } = props;
+  const isVerified = true;
+
+  const renderStatusElements = () => {
+    if (isVerified) {
+      return (
+        <>
+          <Ionicons name='checkmark-circle' size={64} color={'green'} />
+          <Spacer height={20} />
+          <Text style={styles.verificationStatusText}>Email Verified!</Text>
+        </>
+      );
+    }
+    return (
+      <>
+        <Ionicons name='sync' size={64} color='#2f2e41' />
+        <Spacer height={20} />
+        <Text style={styles.verificationStatusText}>
+          Awaiting Email Verification
+        </Text>
+      </>
+    );
+  };
+
+  const getButtonProps = () => {
+    if (isVerified) {
+      return {
+        ctaTitle: 'Next',
+        ctaCallback: () => setStep(SignupStep.SELECT_HOUSEHOLD),
+      };
+    }
+    return {
+      ctaTitle: 'Open Email App',
+      ctaCallback: () => {},
+    };
+  };
 
   return (
-    <ScreenWrapper
-      title='Verify Email'
-      ctaTitle='Open EmailApp'
-      ctaCallback={() => setStep(SignupStep.SELECT_HOUSEHOLD)}
-    >
-      <View>
-        <Text>UI Elements Go Here</Text>
+    <ScreenWrapper title='Verify Email' {...getButtonProps()}>
+      <View style={styles.verificationStatusWrapper}>
+        {renderStatusElements()}
       </View>
     </ScreenWrapper>
   );
 };
+
+const styles = StyleSheet.create({
+  verificationStatusWrapper: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  verificationStatusText: {
+    fontSize: 24,
+    color: '#2f2e41',
+  },
+});
 
 export default VerifyEmail;
