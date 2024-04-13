3a001cd6d609372c77a57243fd14c87978b0b6dd
Dave Gentilli <dgentilli@gmail.com>
2024-04-13 17:00:44 +0300

    Create mock data for lists

---
+++ mockData/groceries.js
@@ -0,0 +1,44 @@
+export const groceriesMockData = [
+  {
+    id: '0',
+    itemName: 'Milk',
+    itemQuantity: 1,
+    isChecked: false,
+  },
+  {
+    id: '1',
+    itemName: 'Bread',
+    itemQuantity: 1,
+    isChecked: false,
+  },
+  {
+    id: '2',
+    itemName: 'Eggs',
+    itemQuantity: 10,
+    isChecked: false,
+  },
+  {
+    id: '3',
+    itemName: 'Butter',
+    itemQuantity: 1,
+    isChecked: false,
+  },
+  {
+    id: '4',
+    itemName: 'Rice',
+    itemQuantity: 1,
+    isChecked: false,
+  },
+  {
+    id: '5',
+    itemName: 'Chocoloate',
+    itemQuantity: 2,
+    isChecked: false,
+  },
+  {
+    id: '6',
+    itemName: 'Potato',
+    itemQuantity: 5,
+    isChecked: false,
+  },
+];
---
+++ mockData/pharmacy.js
@@ -0,0 +1,20 @@
+export const pharmacyMockData = [
+  {
+    id: '0',
+    itemName: 'Dymista',
+    itemQuantity: 1,
+    isChecked: false,
+  },
+  {
+    id: '1',
+    itemName: 'Paracetemol',
+    itemQuantity: 1,
+    isChecked: false,
+  },
+  {
+    id: '2',
+    itemName: 'Vitamins',
+    itemQuantity: 1,
+    isChecked: false,
+  },
+];
