bc61bfabb8b60ee3af7dc8bcc1fa5ae73c550639
Dave Gentilli <dgentilli@gmail.com>
2024-04-21 14:28:09 +0300

    Implement signin with email and password

--- screens/SigninScreen/SigninScreenContainer.tsx
+++ screens/SigninScreen/SigninScreenContainer.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import SigninScreenUI from './SigninScreenUI';
+import { useNavigation } from '@react-navigation/native';
 const MemoizedSignupScreenUI = React.memo(SigninScreenUI);
 
 const SigninScreenContainer = () => {
-  return <MemoizedSignupScreenUI />;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation<any>();
+  // To Do: Add TS to the navigation
+  const auth = getAuth();
+
+  const onPressLink = () => {
+    navigation.navigate('Signup');
+  };
+
+  const onPressCtaButton = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('user from Sign in', user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('signin errorCode', errorCode);
+        console.log('signing errorMessage', errorMessage);
+      });
+  };
+
+  return (
+    <MemoizedSignupScreenUI
+      email={email}
+      password={password}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      onPressCtaButton={onPressCtaButton}
+      onPressLink={onPressLink}
+    />
+  );
 };
 
 export default SigninScreenContainer;
--- screens/SigninScreen/SigninScreenUI.tsx
+++ screens/SigninScreen/SigninScreenUI.tsx
@@ -1,15 +1,83 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import AuthScreenWrapper from '../../baseComponents/AuthScreenWrapper';
+import Spacer from '../../baseComponents/Spacer';
+import Link from '../../baseComponents/Link';
 
-interface SigninScreenProps {}
+interface SigninScreenProps {
+  email: string;
+  password: string;
+  setEmail: (input: string) => void;
+  setPassword: (input: string) => void;
+  onPressLink: () => void;
+  onPressCtaButton: () => void;
+}
 
 const SigninScreenUI = (props: SigninScreenProps) => {
-  const {} = props;
+  const {
+    email,
+    password,
+    setEmail,
+    setPassword,
+    onPressLink,
+    onPressCtaButton,
+  } = props;
 
   return (
-    <View>
-      <Text>SigninScreenUI</Text>
-    </View>
+    <AuthScreenWrapper
+      title='Create Account'
+      ctaTitle='Sign in'
+      ctaCallback={onPressCtaButton}
+    >
+      <Text style={styles.label}>Enter your email address</Text>
+
+      <TextInput
+        style={styles.input}
+        value={email}
+        keyboardType='email-address'
+        placeholder='Email Address'
+        placeholderTextColor='#3f3d56'
+        autoCapitalize='none'
+        autoCorrect={false}
+        onChangeText={(input) => setEmail(input)}
+      />
+
+      <Spacer height={50} />
+
+      <Text style={styles.label}>Enter a password</Text>
+
+      <TextInput
+        style={styles.input}
+        value={password}
+        placeholder='Password'
+        placeholderTextColor='#3f3d56'
+        secureTextEntry
+        autoCapitalize='none'
+        autoCorrect={false}
+        onChangeText={(input) => setPassword(input)}
+      />
+
+      <Spacer height={25} />
+
+      <Link text='Need to Sign up?' onPress={onPressLink} />
+    </AuthScreenWrapper>
   );
 };
+
+const styles = StyleSheet.create({
+  label: {
+    marginBottom: 10,
+    fontSize: 16,
+    color: '#2f2e41',
+  },
+  input: {
+    borderColor: '#e6e6e6',
+    borderWidth: 1,
+    height: 50,
+    width: '100%',
+    borderRadius: 6,
+    padding: 10,
+    textAlign: 'center',
+  },
+});
 
 export default SigninScreenUI;
