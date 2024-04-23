d9d4ee20e07f776608c469d738f6c2acfe5dbd63
Dave Gentilli <dgentilli@gmail.com>
2024-04-23 20:39:34 +0300

    Create a useAppStateHook

---
+++ hooks/useAppState.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from 'react';
+import { AppState } from 'react-native';
+
+const useAppState = () => {
+  const appState = useRef(AppState.currentState);
+  const [currentAppState, setCurrentAppState] = useState(appState.current);
+
+  useEffect(() => {
+    console.log('appState from useEffect', currentAppState);
+  }, [currentAppState]);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      setCurrentAppState(nextAppState);
+    });
+
+    return () => subscription.remove();
+  }, []);
+
+  const onChange = (callback: Function) => {
+    callback();
+  };
+
+  const onForeground = (callback: Function) => {
+    if (currentAppState === 'active') {
+      callback();
+    }
+  };
+
+  const onBackground = (callback: Function) => {
+    if (currentAppState !== 'active') {
+      callback();
+    }
+  };
+
+  return { currentAppState, onChange, onForeground, onBackground };
+};
+
+export default useAppState;
