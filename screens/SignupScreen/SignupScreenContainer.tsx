3c65784a3579bea05b4f613c041344519a0a4e07
Dave Gentilli <dgentilli@gmail.com>
2024-05-15 21:48:56 +0300

    Work on the logic for households

--- screens/SignupScreen/SignupScreenContainer.tsx
+++ screens/SignupScreen/SignupScreenContainer.tsx
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import { uuid } from 'uuidv4';
+import { db } from '../../App';
 import SignupScreenUI from './SignupScreenUI';
 import useAuth from '../../hooks/useAuth';
 import { useNavigation } from '@react-navigation/native';
 import { SignupStep } from '../../constants/signup';
 import useAppState from '../../hooks/useAppState';
 import Validator from '../../services/Validator';
+// import { User } from '../../models/firestore';
+
+const SHARE_CODE_LENGTH = 8;
 
 const MemoizedSignupScreenUI = React.memo(SignupScreenUI);
 
@@ -23,6 +38,7 @@ const SignupScreenContainer = () => {
   const [password, setPassword] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [householdCode, sethouseholdCode] = useState('');
+  const [householdError, setHouseholdError] = useState('');
   const navigation = useNavigation<any>();
 
   const onPressCreateAccount = async () => {
@@ -41,18 +57,64 @@ const SignupScreenContainer = () => {
     setPasswordError(Validator.validatePassword(password));
   };
 
-  const onSelectHousehold = (householdCode: string) => {
-    // if !code.length - generate a new household
-    // generate a share code
-    // create a new household and push it to the DB
-    // the newly created houshold should also have a userId with the user's current id
-    // and the share code
-    // handle an error and display message if needed
-    // if code.length
-    // check db for the code to see if it exists
-    // if it exists, add the user to the household
-    // if it doesn't exist display an error message
-    //once there are no errors, navigate to the next screen
+  const generateShareCode = () => {
+    const chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    console.log('chars from generate code', chars);
+    let result = '';
+    for (let i = 0; i < SHARE_CODE_LENGTH; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  };
+
+  const onSelectHousehold = async (householdCode: string) => {
+    if (!currentUser) return;
+    try {
+      let householdId;
+      if (!householdCode || householdCode.length < 1) {
+        const householdData = {
+          id: uuid(),
+          shareCode: generateShareCode(),
+          userIds: [currentUser?.uid],
+          listId: [],
+        };
+
+        const householdRef = await addDoc(
+          collection(db, 'households'),
+          householdData
+        );
+        householdId = householdRef.id;
+        const updatedUser = await updateDoc(currentUser as any, {
+          householdId,
+        });
+        console.log('updatedUser - new household', updatedUser);
+        return;
+      }
+
+      const householdsRef = collection(db, 'households');
+      const q = query(householdsRef, where('shareCode', '==', householdCode));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach((doc) => {
+          console.log('doc from querySnapshot', doc);
+          householdId = doc.data().id;
+        });
+      } else {
+        setHouseholdError('Invalid Household');
+      }
+
+      if (householdId) {
+        const updatedUser = await updateDoc(currentUser as any, {
+          householdId,
+        });
+        console.log('updatedUser - existing household', updatedUser);
+      }
+      return;
+    } catch (error) {
+      console.error('error from onSelectHousehold', error);
+    }
   };
 
   useEffect(() => {
