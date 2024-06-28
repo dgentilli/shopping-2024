915e8885f4133a2c596f527141d41e7690254550
Dave Gentilli <dgentilli@gmail.com>
2024-06-28 17:19:13 +0300

    Add the necessary logic to the pharmacy screen

--- screens/PharmacyScreen/PharmacyScreenContainer.tsx
+++ screens/PharmacyScreen/PharmacyScreenContainer.tsx
@@ -1,21 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PharmacyScreenUI from './PharmacyScreenUI';
-import { pharmacyMockData } from '../../mockData/pharmacy';
 import { ListItemType } from '../../constants/listItemType';
+import useAuth from '../../hooks/useAuth';
+import { collection, doc, getDocs, onSnapshot } from 'firebase/firestore';
+import { db } from '../../App';
 
 const MemoizedPharmacyScreenUI = React.memo(PharmacyScreenUI);
 
 const PharmacyScreenContainer = () => {
-  const data: ListItemType[] = pharmacyMockData;
+  const { currentUser } = useAuth();
+  const [data, setData] = useState<ListItemType[]>([]);
+  const [error, setError] = useState('');
 
-  const deleteItem = (id: string, type: 'grocery' | 'pharmacy') => {
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!currentUser) return;
+
+      const householdsRef = collection(db, 'households');
+      const querySnapshot = await getDocs(householdsRef);
+      const userHousehold = querySnapshot.docs.find((doc) => {
+        const data = doc.data();
+        return data.householdData.userIds.includes(currentUser.uid);
+      });
+
+      if (!userHousehold) return;
+
+      const unsub = onSnapshot(
+        doc(db, 'households', userHousehold.id),
+        (doc) => {
+          if (doc.exists()) {
+            const householdData = doc.data();
+            const listData = householdData.lists?.pharmacy || [];
+            setData(listData);
+          } else {
+            setError(
+              'There was a problem retrieving your list. Please try again'
+            );
+          }
+        }
+      );
+
+      return () => unsub();
+    };
+
+    fetchData();
+  }, [currentUser]);
+
+  const deleteItem = (id: string) => {
     // You'll need to make an API call eventually
     // For now...
     console.log('id recd by deleteItem function', id);
-    console.log('type recd by deleteItem function', type);
   };
 
-  return <MemoizedPharmacyScreenUI data={data} deleteItem={deleteItem} />;
+  return (
+    <MemoizedPharmacyScreenUI
+      data={data}
+      error={error}
+      deleteItem={deleteItem}
+    />
+  );
 };
 
 export default PharmacyScreenContainer;
--- screens/PharmacyScreen/PharmacyScreenUI.tsx
+++ screens/PharmacyScreen/PharmacyScreenUI.tsx
@@ -4,15 +4,16 @@ import ListScreenWrapper from '../../components/ListComponents/ListScreenWrapper
 
 interface PharmacyScreenProps {
   data: ListItemType[];
-  deleteItem: (id: string, type: 'grocery' | 'pharmacy') => void;
+  error: string;
+  deleteItem: (id: string) => void;
 }
 
 const PharmacyScreenUI = (props: PharmacyScreenProps) => {
-  const { data, deleteItem } = props;
+  const { data, error, deleteItem } = props;
 
   return (
     <ListScreenWrapper title='Pharmacy' type='pharmacy'>
-      <List data={data} deleteItem={deleteItem} />
+      <List data={data} error={error} deleteItem={deleteItem} />
     </ListScreenWrapper>
   );
 };
