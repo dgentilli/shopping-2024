77f0676239b56d60aab6933d4f186758d794d5f7
Dave Gentilli <dgentilli@gmail.com>
2024-07-05 19:16:56 -0400

    Add icons and tab bar colors

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Ionicons from '@expo/vector-icons/Ionicons';
 import GroceriesScreenContainer from '../screens/GroceriesScreen/GroceriesScreenContainer';
 import PharmacyScreenContainer from '../screens/PharmacyScreen/PharmacyScreenContainer';
 import SettingsScreenContainer from '../screens/SettingsScreen/SettingsScreenContainer';
@@ -9,10 +9,51 @@ const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Groceries' component={GroceriesScreenContainer} />
-      <Tab.Screen name='Pharmacy' component={PharmacyScreenContainer} />
-      <Tab.Screen name='Settings' component={SettingsScreenContainer} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#6c63ff',
+        tabBarInactiveTintColor: '#847a85',
+      }}
+    >
+      <Tab.Screen
+        name='Groceries'
+        component={GroceriesScreenContainer}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'cart' : 'cart-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Pharmacy'
+        component={PharmacyScreenContainer}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'medical' : 'medical-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Settings'
+        component={SettingsScreenContainer}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'cog-sharp' : 'cog-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
